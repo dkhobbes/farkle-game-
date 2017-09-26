9c5787e139f8aadb1301660cd4b2e914d1c30f0f
David Kurth <david-kurth@sbcglobal.net>
2017-09-26 07:30:54 -0500

    adding initial js logic

---
+++ script.js
@@ -0,0 +1,27 @@
+//Select random numbers per die
+//add in score of selected by click
+//save score, and continue to roll
+
+
+//To win at Farkle you must be the player with the highest score above 10,000 points on the final round of play.
+
+/*
+1 Spot 100
+5 Spot 50
+
+Combinations
+
+Three 2 Spot 200
+Three 3 Spot 300
+Three 4 Spot 400
+Three 5 Spot 500
+Three 6 Spot 600
+Three 1 Spot 1,000
+Three Pairs 500
+Six-Dice Straight 1,000
+Three Farkles Lose 1,000 */
+
+var board = function() {
+  var runTotal = document.getElementById('currentTotal');
+  var allDice = [];
+}
